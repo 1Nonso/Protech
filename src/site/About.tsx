482aeaafa38c976095221d11d751babb32b69e52
mac <chukchi2345@gmail.com>
2025-07-24 18:32:18 +0100

    Added and styled protech description, animation included

--- src/site/About.tsx
+++ src/site/About.tsx
@@ -1,7 +1,53 @@
+import sample from "./../assets/sample.png";
+import { motion } from "framer-motion";
+
 const About = () => {
   return (
-    <section id="about" className="h-200">
-      Hello, This is the about section
+    <section id="about" className="w-dvw">
+      <div className="bg-white text-[#0099FF] w-full pt-5 flex flex-col items-center overflow-hidden">
+        <h5 className="font-bold text-5xl mb-5 w-[95%] ">about us</h5>
+        <div className="w-full flex flex-col md:flex-row items-center justify-evenly">
+          <motion.img
+            initial={{ x: -250, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.25, delay: 0.7 }}
+            viewport={{ amount: 0.0, once: true }}
+            src={sample}
+            alt="image of a worker"
+            className="w-[80%] md:w-[30%] rounded-lg mb-5"
+          />
+          <motion.p
+            className="text-lg text w-[90%] md:w-[50%] font-[roboto] font-semibold text-left "
+            initial={{ opacity: 0 }}
+            viewport={{ once: true }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 0.7 }}
+          >
+            <span className="font-bold text-xl">
+              ProTech is a future-driven tech collective, built to shape how
+              businesses connect, grow, and protect in the digital age.
+            </span>
+            <span className="block text-center text-xl my-5 font-[borel] italic ">
+              creativity?, no bounds
+            </span>
+            We craft sleek, intuitive websites and mobile apps tailored to the
+            pulse of modern business. But we don`t stop at launch — our data
+            analysis services help companies uncover patterns, predict trends,
+            and make smarter moves. And in a world full of digital threats,
+            we`ve made cybersecurity part of the blueprint — not an
+            afterthought.
+            <span className="block text-center text-xl my-5 font-[borel] italic">
+              Our client, always in the spotlight
+            </span>
+            ProTech was born to solve real-world problems with bold,
+            custom-built solutions. From UX design to backend architecture, our
+            team blends design thinking, technical mastery, and a deep respect
+            for digital trust. Wherever your business is headed, we`re already
+            building the road to get there.{" "}
+          </motion.p>
+        </div>
+      </div>
+      <div></div>
     </section>
   );
 };
