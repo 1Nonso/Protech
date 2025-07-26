9900dba31a747ce79c66874d227d30bf29a37783
mac <chukchi2345@gmail.com>
2025-07-26 19:46:21 +0100

    Added element and restyled component

--- src/site/Contact.tsx
+++ src/site/Contact.tsx
@@ -8,43 +8,48 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="flex flex-col md:flex-row items-center justify-center md:justify-evenly bg-[#000b22] w-full pt-50 "
+      className="flex flex-col items-center justify-center md:justify-evenly bg-[#000b22] w-full pt-50 "
     >
-      <div className="md:w-[50%] flex flex-col items-center justify-center ">
-        <h5 className="text-white text-5xl md:text-7xl w-[85%] md:w-full ">
-          we're really excited about this
-        </h5>
-        {isMobile && (
+      <div className="flex items-center justify-evenly w-full">
+        <div className="md:w-[50%] flex flex-col items-center justify-center ">
+          <h5 className="text-white text-5xl md:text-7xl w-[85%] md:w-full ">
+            we're really excited about this
+          </h5>
+          {isMobile && (
+            <img
+              src={contactImage}
+              alt="image of a macbook with figma open"
+              className="w-[85%] md:w-[30%] rounded-lg my-10 "
+            />
+          )}
+          <p className="text-xl md:text-2xl text-center md:text-left my-5 text-white">
+            Working with brands that refuse to blend in. If you want to break
+            rules, challenge norms, and create work that actually matters, let's
+            talk. Our global team is standing by, always ready for a challenge.
+          </p>
+          <div className="text-white ">
+            <p className="text-2xl underline my-5">Lets get started</p>
+            <div className="flex items-center my-5">
+              <img src={emailIcon} alt="phone Icon" className="w-10 pr-2" />
+              <p>Email: protechnigltd6@gmail.com </p>
+            </div>
+            <div className="flex items-center my-5">
+              <img src={phoneIcon} alt="Email Icon" className="w-10 pr-2" />
+              <p>Phone Number: +234 7010698493</p>
+            </div>
+          </div>
+        </div>
+        {!isMobile && (
           <img
             src={contactImage}
             alt="image of a macbook with figma open"
             className="w-[85%] md:w-[30%] rounded-lg my-10 "
           />
         )}
-        <p className="text-xl md:text-2xl text-center md:text-left my-5 text-white">
-          Working with brands that refuse to blend in. If you want to break
-          rules, challenge norms, and create work that actually matters, let's
-          talk. Our global team is standing by, always ready for a challenge.
-        </p>
-        <div className="text-white ">
-          <p className="text-2xl underline my-5">Lets get started</p>
-          <div className="flex items-center my-5">
-            <img src={emailIcon} alt="phone Icon" className="w-10 pr-2" />
-            <p>Email: protechnigltd6@gmail.com </p>
-          </div>
-          <div className="flex items-center my-5">
-            <img src={phoneIcon} alt="Email Icon" className="w-10 pr-2" />
-            <p>Phone Number: +234 7010698493</p>
-          </div>
-        </div>
       </div>
-      {!isMobile && (
-        <img
-          src={contactImage}
-          alt="image of a macbook with figma open"
-          className="w-[85%] md:w-[30%] rounded-lg my-10 "
-        />
-      )}
+      <p className="text-white my-25 font-[borel] text-3xl ">
+        Protech. Bringing bold ideas to life
+      </p>
     </section>
   );
 };
